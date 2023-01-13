40ccb76ce2eae788db6014cb5293f722501794ee
joaogabrielvalenca <joaogabrielvalenca@gmail.com>
2023-01-13 10:33:42 -0300

    pegando 95% getOpeningHours

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -35,4 +35,28 @@ describe('Testes da função getOpeningHours', () => {
     const wednesdayReturn = 'The zoo is closed';
     expect(functionWednesday).toEqual(wednesdayReturn);
   });
+  it('com o parâmetro sendo "thu" e 09:00-PM retorna um erro', () => {
+    const thu = 'Thu';
+    const thuHour = '09:00-PM';
+    const functionThu = getOpeningHours(thu, thuHour);
+    expect(functionThu).toThrow(Error);
+  });
+  it('com o parametro sendo Friday e 09:00-ZM lança um erro', () => {
+    const friday = 'Friday';
+    const fridayHour = '09:00-ZM';
+    const functionFriday = getOpeningHours(friday, fridayHour);
+    expect(functionFriday).toThrow(Error);
+  });
+  it('com o parâmetro sendo Saturday e o C9:00-AM lança um erro', () => {
+    const saturday = 'Saturday';
+    const saturdayHour = 'C9:00-AM';
+    const functionSaturday = getOpeningHours(saturday, saturdayHour);
+    expect(functionSaturday).toThrow(Error);
+  });
+  it('com o parâmetro sendo Sunday e 09:c0-AM lança um erro', () => {
+    const sunday = 'Sunday';
+    const sundayHour = '09:c0:AM';
+    const functionSunday = getOpeningHours(sunday, sundayHour);
+    expect(functionSunday).toThrow(Error);
+  });
 });
